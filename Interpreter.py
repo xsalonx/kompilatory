8cd9398de52c9efc4efef23701dbd27915d7b723
Ela Cymerys <ela.cymerys@gmail.com>
2022-01-08 21:38:19 +0100

    Prepare an interpreter file

--- Interpreter.py
+++ Interpreter.py
@@ -1,18 +1,115 @@
 import AST
 import SymbolTable
 from Memory import *
-from Exceptions import  *
+from Exceptions import *
 from visit import *
 import sys
 
 sys.setrecursionlimit(10000)
 
+
 class Interpreter(object):
 
+    def __init__(self):
+        pass
 
     @on('node')
     def visit(self, node):
         pass
+
+    @when(AST.Node)
+    def visit(self, node):
+        pass
+
+    @when(AST.Scope)
+    def visit(self, node):
+        pass
+
+    @when(AST.IntNum)
+    def visit(self, node):
+        pass
+
+    @when(AST.FloatNum)
+    def visit(self, node):
+        pass
+
+    @when(AST.String)
+    def visit(self, node):
+        pass
+
+    @when(AST.Variable)
+    def visit(self, node):
+        pass
+
+    @when(AST.BinaryExpr)
+    def visit(self, node):
+        pass
+
+    @when(AST.Range)
+    def visit(self, node):
+        pass
+
+    @when(AST.Ref)
+    def visit(self, node):
+        pass
+
+    @when(AST.UnaryMinus)
+    def visit(self, node):
+        pass
+
+    @when(AST.UnaryTranspose)
+    def visit(self, node):
+        pass
+
+    @when(AST.IfStatement)
+    def visit(self, node):
+        pass
+
+    @when(AST.ForLoop)
+    def visit(self, node):
+        pass
+
+    @when(AST.WhileLoop)
+    def visit(self, node):
+        pass
+
+    @when(AST.BreakInstruction)
+    def visit(self, node):
+        pass
+
+    @when(AST.ContinueInstruction)
+    def visit(self, node):
+        pass
+
+    @when(AST.ReturnStatement)
+    def visit(self, node):
+        pass
+
+    @when(AST.Printable)
+    def visit(self, node):
+        pass
+
+    @when(AST.PrintStatement)
+    def visit(self, node):
+        pass
+
+    @when(AST.MatrixSpecialWord)
+    def visit(self, node):
+        pass
+
+    @when(AST.Vector)
+    def visit(self, node):
+        pass
+
+    @when(AST.Matrix)
+    def visit(self, node):
+        pass
+
+    @when(AST.Error)
+    def visit(self, node):
+        pass
+
+    """
 
     @when(AST.BinOp)
     def visit(self, node):
@@ -35,5 +132,5 @@ class Interpreter(object):
         while node.cond.accept(self):
             r = node.body.accept(self)
         return r
-
-
+        
+    """
