0d75a5ba78287ec278bdd4d2a7a888f21f9f6c0f
Ela Cymerys <ela.cymerys@gmail.com>
2022-01-08 19:56:19 +0100

    Create a project structure

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-
 import sys
 import ply.yacc as yacc
 import Mparser
 import scanner
 from TreePrinter import TreePrinter
 from TypeChecker import TypeChecker
+from Interpreter import Interpreter
+
 
 if __name__ == '__main__':
 
     try:
-        filename = sys.argv[1] if len(sys.argv) > 1 else "opers.m"
+        filename = sys.argv[1] if len(sys.argv) > 1 else "fibonacci.m"
         file = open(filename, "r")
     except IOError:
         print("Cannot open {0} file".format(filename))
@@ -20,9 +21,13 @@ if __name__ == '__main__':
 
     ast = parser.parse(text, lexer=scanner.lexer)
 
-    # if not Mparser.is_error:
-        # ast.printTree()
-
     # Below code shows how to use visitor
-    typeChecker = TypeChecker()
+    typeChecker = TypeChecker()   
     typeChecker.visit(ast)   # or alternatively ast.accept(typeChecker)
+
+    ast.accept(Interpreter())
+    # in future
+    # ast.accept(OptimizationPass1())
+    # ast.accept(OptimizationPass2())
+    # ast.accept(CodeGenerator())
+    
